a5d77aae544bd94c9a9c7c826a8ec2f83215029c
Mohd Umar <uk1941404@gmail.com>
2025-06-28 22:57:00 +0530

    fix cors origin

--- server/config/allowedOrigin.js
+++ server/config/allowedOrigin.js
@@ -1,6 +1,6 @@
 export const allowOrigin = [
-    'https://agent-system-bp82.onrender.com/api',
+    'https://agent-system-bp82.onrender.com', // ✅ no `/api` at the end
     'http://localhost:5173',
     'https://agent-system-chi.vercel.app'
-]
-
+  ];
+  
